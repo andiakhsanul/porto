c7486ef70e81a4a78e386138a02b78fd48fa5582
andi akhsanul <116082109+andiakhsanul@users.noreply.github.com>
2025-08-23 20:26:22 +0700

    Add app configuration, views, and URL routing for the Django project

--- webporto/webporto/settings.py
+++ webporto/webporto/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app.apps.AppMainConfig',  # Register the app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
